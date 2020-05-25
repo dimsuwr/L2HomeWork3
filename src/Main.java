bbc53d25a655ab59122fa142e8f0f44affeb0a70
dimsuwr <soup_95@mail.ru>
2020-05-25 08:29:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+import java.util.TreeMap;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+                String[] world = new String[10];
+                world[0] = "Набор_1";
+                world[1] = "Набор_1";
+                world[2] = "Набор_1";
+                world[3] = "12345";
+                world[4] = "Набо_2";
+                world[5] = "Набор_3";
+                world[6] = "Набор_4";
+                world[7] = "12";
+                world[8] = "Набор_3";
+                world[9] = "Финал";
+
+               uniqueMethod(world);
+
+                guide();
+            }
+
+            public static void uniqueMethod(String[] world){
+
+
+                TreeMap<String, Integer> treeMap = new TreeMap<>();
+
+                for (int i = 0; i< world.length; i++){
+                    if (treeMap.containsKey(world[i])){
+                        treeMap.put(world[i], treeMap.get(world[i])+1);
+                    }else{
+                        treeMap.put(world[i], 1);
+                    }
+                }
+
+                System.out.println(treeMap);
+            }
+
+            public static void guide(){
+
+
+                PhoneList phoneList = new PhoneList();
+
+                phoneList.add("Дмитрий", "79150000001");
+                phoneList.add("Екатерина", "79150000002");
+                phoneList.add("Юлия", "79150000003");
+                phoneList.add("Ирина", "79150000004");
+                phoneList.add("Владимир", "79150000005");
+                phoneList.add("Юлия", "79150000006");
+                phoneList.add("Дмитрий", "79150000007");
+                phoneList.add("Дмитрий", "79150000008");
+
+                System.out.println(phoneList.get("Дмитрий"));
+                System.out.println(phoneList.get("Алексей"));
+                System.out.println(phoneList.get("Екатерина"));
+                System.out.println(phoneList.get("Юлия"));
+                System.out.println(phoneList.get("Владимир"));
+    }
+}
